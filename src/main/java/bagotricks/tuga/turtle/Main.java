ee0f6ba2b68d3a3e51e25ad0061bd4be6aa94a99
Rafael Bedia <dcat@trillinux.org>
2015-11-14 20:59:23 -0500

    Formatted.

--- src/main/java/bagotricks/tuga/turtle/Main.java
+++ src/main/java/bagotricks/tuga/turtle/Main.java
@@ -9,25 +9,25 @@ import bagotricks.tuga.turtle.examples.Examples;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e) {
-				// TODO No system laf. Oh well.
-			}
-			MainUi ui = new MainUi();
-			ui.firstContent = Examples.getContent("Angle Patterns");
-			ui.engine = new TurtleEngine();
-			ui.engine.init();
-			ui.examples = Examples.getAll();
-			ui.icon = ImageIO.read(Main.class.getResource("turtle128.png"));
-			ui.id = "Tuga Turtle";
-			ui.title = "Tuga Turtle (from Bagotricks.com)";
-			SwingUtilities.invokeAndWait(ui);
-		} catch (Exception e) {
-			Thrower.throwAny(e);
-		}
-	}
+    public static void main(String[] args) {
+        try {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                // TODO No system laf. Oh well.
+            }
+            MainUi ui = new MainUi();
+            ui.firstContent = Examples.getContent("Angle Patterns");
+            ui.engine = new TurtleEngine();
+            ui.engine.init();
+            ui.examples = Examples.getAll();
+            ui.icon = ImageIO.read(Main.class.getResource("turtle128.png"));
+            ui.id = "Tuga Turtle";
+            ui.title = "Tuga Turtle (from Bagotricks.com)";
+            SwingUtilities.invokeAndWait(ui);
+        } catch (Exception e) {
+            Thrower.throwAny(e);
+        }
+    }
 
 }
